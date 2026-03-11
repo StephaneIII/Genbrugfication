e8c2ad74b058cc13e4b0316308ed692a6d6edf7d
StephaneIII <stk002@edu.zealand.dk>
2026-03-11 13:38:06 +0100

    Added vuetify

--- src/models/main.ts
+++ src/models/main.ts
@@ -2,8 +2,23 @@ import { createApp } from 'vue'
 import App from '../vue/App.vue'
 import router from '../router'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Material Design Icons
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 const app = createApp(App)
 
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
--- src/vue/App.vue
+++ src/vue/App.vue
@@ -1,11 +1,42 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row justify="center">
+          <v-col cols="12" md="8" class="text-center">
+            <h1 class="text-h3 mb-4">You did it!</h1>
+            <v-card class="pa-6" elevation="2">
+              <v-card-text>
+                <p class="text-h6 mb-4">Your Vue.js application with Vuetify is now ready!</p>
+                <v-btn
+                  href="https://vuejs.org/"
+                  target="_blank"
+                  color="primary"
+                  variant="elevated"
+                  prepend-icon="mdi-vuejs"
+                  class="ma-2"
+                >
+                  Vue.js Documentation
+                </v-btn>
+                <v-btn
+                  href="https://vuetifyjs.com/"
+                  target="_blank"
+                  color="secondary"
+                  variant="elevated"
+                  prepend-icon="mdi-vuetify"
+                  class="ma-2"
+                >
+                  Vuetify Documentation
+                </v-btn>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped></style>
