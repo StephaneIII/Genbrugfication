d3ca9eadc2cb5e6a10c511113d7b1a1b5c8a56b8
Lucas Valls-Sjøstrand <Lucasvallssj@gmail.com>
2026-04-20 17:27:11 +0200

    Controller skeleton
    Made the skeletons for the controllers for Checkout & CheckoutItem

--- api/src/controllers/Checkout.controlller.ts
+++ api/src/controllers/Checkout.controlller.ts
@@ -1,1 +1,34 @@
 import { Request, Response } from 'express'
+import User from '../models/checkout.model.js'
+
+export const createCheckout = async (req: Request, res: Response): Promise<void> => {
+    try {
+        
+    } catch (error) {
+
+    }
+}
+
+export const getCheckoutById = async (req: Request, res: Response): Promise<void> => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+export const updateCheckout = async (req: Request, res: Response): Promise<void> => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+export const deleteCheckout = async (req: Request, res: Response): Promise<void> => {
+    try {
+
+    } catch (error) {
+
+    }
+}
--- api/src/controllers/CheckoutItem.controller.ts
+++ api/src/controllers/CheckoutItem.controller.ts
@@ -1,1 +1,50 @@
 import { Request, Response } from 'express'
+import User from '../models/checkout-item.model.js'
+
+export const createCheckoutItem = async (req: Request, res: Response): Promise<void> => {
+    try {
+        
+    } catch (error) {
+
+    }
+}
+
+export const getAllCheckoutItems = async (req: Request, res: Response): Promise<void> => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+export const getCheckoutItemById = async (req: Request, res: Response): Promise<void> => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+export const updateCheckoutItem = async (req: Request, res: Response): Promise<void> => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+export const deleteCheckoutItem = async (req: Request, res: Response): Promise<void> => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+export const deleteAllCheckoutItem = async (req: Request, res: Response): Promise<void> => {
+    try {
+
+    } catch (error) {
+        
+    }
+}
