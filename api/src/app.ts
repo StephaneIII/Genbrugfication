6a7b5ffb50fcac7dc86569e38908a714f68cc02e
HammerLasse <lasse@thomasminde.dk>
2026-04-23 09:59:43 +0200

    glemte stops i app

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 import apiRouter from './routes/index.js'
 import { notFound } from './middleware/notFound.js'
 import { errorHandler } from './middleware/errorHandler.js'
+import stopsRoutes from "./routes/Stops.routes.js";
 
 const app = express()
 
@@ -10,8 +11,12 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', apiRouter)
+app.use('/api/', stopsRoutes)
+
 
 app.use(notFound)
 app.use(errorHandler)
 
 export default app
+
+
